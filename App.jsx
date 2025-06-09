59c7fc756eb4759de816af863f11d4b1c6f69fd4
amarjeetkumar2012199 <amarjeetkumar2012199@gmail.com>
2025-06-09 16:40:41 +0530

    src

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import StudentForm from './components/StudentForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
